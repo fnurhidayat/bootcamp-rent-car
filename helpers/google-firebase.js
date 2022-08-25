5961941c9613e42924b482fb07de8bd485b34aa3
enricoanto <enricoanto@yahoo.com>
2022-08-25 09:05:08 +0700

    setting girebase

--- helpers/google-firebase.js
+++ helpers/google-firebase.js
@@ -10,7 +10,7 @@ const serviceAccount = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-h4dos%40rent-car-507f6.iam.gserviceaccount.com"
+  "client_x509_cert_url": process.env.CLIENT_CERT_URL
 }
 
 module.exports = serviceAccount
